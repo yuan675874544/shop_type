cf838d9c0e09c8fd9f583a5e95782d28193aab88
yuanyazhou <675874544@qq.com>
2021-01-18 10:36:18 +0800

    这是属性值维护文档接口

--- src/main/java/com/fh/api/type/shop_type/controller/ShuValueController.java
+++ src/main/java/com/fh/api/type/shop_type/controller/ShuValueController.java
@@ -18,19 +18,42 @@ import java.util.Map;
 public class ShuValueController {
     @Resource
     private ShuValueService shuValueService;
-    //查询
+    /*查询
+     * 路径   127.0.0.1:8080/ShuValueController/queryAll
+     *
+     * 参数 attId
+     *
+     * get请求
+     *
+     * */
     @GetMapping("queryAll")
     public ResultData queryAll(Integer attId){
         List<ShuValue> list=shuValueService.queryAll(attId);
         return ResultData.success(list);
     }
     //删除
+    /*
+     * 路径   127.0.0.1:8080/ShuValueController/deleteIsdel
+     *
+     * 参数 vid
+     *
+     * post请求
+     *
+     * */
     @PostMapping("deleteIsdel")
     public     ResultData deleteIsdel(Integer vid){
         shuValueService.deleteIsdel(vid);
         return ResultData.success(null);
     }
     //新增
+    /*
+     * 路径   127.0.0.1:8080/ShuValueController/add
+     *
+     * 参数 name nameCh attId
+     *
+     * post请求
+     *
+     * */
     @PostMapping("add")
     public  Map add(ShuValue shuValue){
         Map map=new HashMap();
@@ -39,11 +62,28 @@ public class ShuValueController {
         return map;
     }
     //根据id去查询单条数据
+    /*
+     * 路径   127.0.0.1:8080/ShuValueController/getDataByid
+     *
+     * 参数：vid
+     *
+     *
+     * get请求
+     *
+     * */
     @GetMapping("getDataByid")
     public  ResultData getDataByid(Integer vid){
         return ResultData.success(shuValueService.getDataByid(vid));
     }
     //修改
+    /*
+     * 路径   127.0.0.1:8080/ShuValueController/update
+     *
+     * 参数：id name nameCH vid(必传)
+     *
+     * post请求
+     *
+     * */
     @PostMapping("update")
     public ResultData update(ShuValue shuValue){
         shuValueService.update(shuValue);
